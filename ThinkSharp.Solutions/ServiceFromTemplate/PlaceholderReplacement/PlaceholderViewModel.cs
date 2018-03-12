d38c1f7d3d67e5baad9ea5b47f3524942023d0a4
Jan-Niklas <JanDotNet@ThinkSharp.de>
2018-03-12 21:32:05 +0100

    moved file PlaceHolderViewModel

--- ThinkSharp.Solutions/ServiceFromTemplate/PlaceholderReplacement/PlaceholderViewModel.cs
+++ ThinkSharp.Solutions/ServiceFromTemplate/PlaceholderReplacement/PlaceholderViewModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ThinkSharp.Solutions.ServiceFromTemplate
+namespace ThinkSharp.Solutions.ServiceFromTemplate.PlaceholderReplacement
 {
     public class PlaceholderViewModel : ViewModelBase
     {
