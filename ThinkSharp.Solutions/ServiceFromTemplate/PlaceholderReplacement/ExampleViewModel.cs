0b6301a770dc646145bb96201b9a4580c7dbf716
Jan-Niklas <JanDotNet@ThinkSharp.de>
2018-03-13 06:52:06 +0100

    added ExampleViewModel

---
+++ ThinkSharp.Solutions/ServiceFromTemplate/PlaceholderReplacement/ExampleViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkSharp.Solutions.Infrastructure;
+using ThinkSharp.Solutions.TemplateDefinition;
+
+namespace ThinkSharp.Solutions.ServiceFromTemplate.PlaceholderReplacement
+{
+    public class ExampleViewModel : ViewModelBase
+    {
+        private readonly Example myExample;
+
+        private string myText;
+
+        public ExampleViewModel(Example example)
+        {
+            myExample = example;
+            myText = myExample.Text;
+        }
+
+        public string Header => myExample.Header;
+
+        public string Text
+        {
+            get { return myText; }
+            set
+            {
+                if (myText == value)
+                    return;
+                myText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        internal void UpdateText(IEnumerable<PlaceholderViewModel> placeHolders)
+        {
+            var text = myExample.Text;
+            foreach (var placeHolder in placeHolders)
+            {
+                if (!string.IsNullOrEmpty(placeHolder.Replacement))
+                {
+                    text = text.Replace(placeHolder.TextToReplace, placeHolder.Replacement);
+                }
+            }
+            Text = text;
+        }
+    }
+}
