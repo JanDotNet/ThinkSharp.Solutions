1356131272598302a8ff1c756db98636fd6dd4c3
Jan-Niklas <JanDotNet@ThinkSharp.de>
2018-03-25 20:21:41 +0200

    added support for branches improved error handling

--- ThinkSharp.Solutions/ServiceFromTemplate/CloneRepository/CloneRepositoryViewModel.cs
+++ ThinkSharp.Solutions/ServiceFromTemplate/CloneRepository/CloneRepositoryViewModel.cs
@@ -15,6 +15,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using log4net;
+using System.Text.RegularExpressions;
 
 namespace ThinkSharp.Solutions.ServiceFromTemplate.CloneRepository
 {
@@ -27,7 +28,9 @@ namespace ThinkSharp.Solutions.ServiceFromTemplate.CloneRepository
             TargetDirectory = Settings.Default.TargetDirectory;
         }
 
-        public string GitRepository { get; set; }        
+        public string GitRepository { get; set; }
+
+        public string GitBranch { get; set; }
 
         private string myTargetDirectory = string.Empty;
         public string TargetDirectory
@@ -78,44 +81,116 @@ namespace ThinkSharp.Solutions.ServiceFromTemplate.CloneRepository
             {
                 MessageBox.Show("Target directory must be empty!", "Info", MessageBoxButton.OK, MessageBoxImage.Hand);
                 return false;
-            }            
+            }
 
             progress.Report("Cloning Repository...");
 
-            string gitRepoDir = null;
-            try
-            {
-                // try to clone without authentication
-                await Task.Run(() =>
-                {
-                    gitRepoDir = Repository.Clone(GitRepository, TargetDirectory);
-                    IOHelper.NormalizeAttributes(gitRepoDir);
-                    Directory.Delete(gitRepoDir, true);
-                });
-            }
-            // probably authentication exception
-            catch(Exception ex)
-            {
-                theLogger.Info($"Unable to clone repository '{GitRepository}' without credentials.", ex);
-                var userName = string.IsNullOrWhiteSpace(Settings.Default.UserName) ? Environment.UserName : Settings.Default.UserName;
-                var vm = new CredentialsViewModel(userName, "Enter Git Credentials");
-                UIHelper.ShowView<CredentialsView>(vm);
-                if (vm.Canceled == true) return false;
-
-                await Task.Run(() =>
-                {
-                    var co = new CloneOptions();
-                    co.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = vm.UserName, Password = vm.Password };
-                    gitRepoDir = Repository.Clone(GitRepository, TargetDirectory, co);
-                });
-            }
-
-            IOHelper.NormalizeAttributes(gitRepoDir);
-            Directory.Delete(gitRepoDir, true);
+            if (!await CloneRepositoryAsync(requestCredentials: false))
+                return false;
 
             ctx.TargetDirectory = TargetDirectory;
 
             return true;
+        }
+
+        private async Task<bool> CloneRepositoryAsync(bool requestCredentials)
+        {
+            string gitRepoDir = null;
+            try
+            {
+                string username = null;
+                string password = null;
+
+                if (requestCredentials)
+                {
+                    theLogger.Info("Requesting Credentials.");
+                    if (!RequestCredentials(out username, out password))
+                    {
+                        return false;
+                    }
+                }
+
+                theLogger.Info("Cloning Repository.");
+                gitRepoDir = await CloneRepositoryAsync(username, password);
+
+                theLogger.Info("Normalizing Attributes.");
+                IOHelper.NormalizeAttributes(gitRepoDir);
+
+                theLogger.Info("Deleting GIT reporitory from working copy.");
+                Directory.Delete(gitRepoDir, true);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // not authenticated
+                if (Regex.IsMatch(ex.Message, "request failed with status code: 401"))
+                {
+                    theLogger.Error("Unable to clone repository: Invalid Credentials. Retry requesting credentials.", ex);
+                    return await CloneRepositoryAsync(requestCredentials: true);
+                }
+                // not found
+                if (Regex.IsMatch(ex.Message, "request failed with status code: 404"))
+                {
+                    var msg = $"Repository '{GitRepository}' does not exist.";
+                    theLogger.Error("Unable to clone repository: " + msg, ex);
+                    MessageBox.Show(msg, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+                // invalid branch name
+                if (Regex.IsMatch(ex.Message, "reference '[^';]*' not found"))
+                {
+                    var msg = $"Branch '{GitBranch}' does not exist.";
+                    theLogger.Error("Unable to clone repository: " + msg, ex);
+                    MessageBox.Show(msg, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+                // invalid protocol
+                if (Regex.IsMatch(ex.Message, "unsupported URL protocol"))
+                {
+                    var msg = $"Unsupported URL protocol. Note that the only supported protocol is HTTP(S).";
+                    theLogger.Error("Unable to clone repository: " + msg, ex);
+                    MessageBox.Show(msg, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+                // unknonw issue
+                else
+                {
+                    MessageBox.Show($"Unable to clone repository: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    theLogger.Error("Unable to clone repository: Unknown Error.", ex);
+                    return false;
+                }
+            }
+        }
+
+        private bool RequestCredentials(out string username, out string password)
+        {
+            username = string.IsNullOrWhiteSpace(Settings.Default.UserName) ? Environment.UserName : Settings.Default.UserName;
+            password = string.Empty;
+
+            var vm = new CredentialsViewModel(username, "Enter Git Credentials");
+            UIHelper.ShowView<CredentialsView>(vm);
+
+            if (vm.Canceled == true) return false;
+
+            username = vm.UserName;
+            password = vm.Password;
+
+            Settings.Default.UserName = username;
+            Settings.Default.Save();
+
+            return true;
+        }
+
+        private Task<string> CloneRepositoryAsync(string username = null, string password = null)
+        {
+            return Task.Run(() =>
+            {
+                var co = new CloneOptions();
+                if (!string.IsNullOrEmpty(GitBranch)) co.BranchName = GitBranch;
+                if (username != null) co.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = username, Password = password };
+                return Repository.Clone(GitRepository, TargetDirectory, co);                    
+            });
         }
 
         public override void SaveSettings()
